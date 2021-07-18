e6ee8ba433c993df36762ec427f00bf374377449
takeyama <nonomi0723@yahoo.co.jp>
2021-07-19 01:46:44 +0900

    上限5までに設定

--- react-project-zenn/src/App.jsx
+++ react-project-zenn/src/App.jsx
@@ -44,7 +44,8 @@ export const App = () => {
 
   return (
     <>
-    <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd} />
+    <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd} disabled={incompleteTodos.length >= 5} />
+    {incompleteTodos.length >= 5 && (<p style={{ color: 'red' }}>登録できるtodo5個まで</p>)}
     <IncompleteTodos todos={incompleteTodos} onClickComplete={onClickComplete} onClickDelete={onClickDelete} />
     <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
   </>
