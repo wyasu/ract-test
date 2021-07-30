f40fae2cea246119166f30dbbf054311e194670f
takeyama <nonomi0723@yahoo.co.jp>
2021-07-31 01:56:47 +0900

    routerの遷移実装

---
+++ react-project-zenn/src/components/Page1.jsx
@@ -0,0 +1,5 @@
+export const Page1 = () => {
+  return (
+    <div><h1>page1です</h1></div>
+  );
+};
---
+++ react-project-zenn/src/components/Page2.jsx
@@ -0,0 +1,5 @@
+export const Page2 = () => {
+  return (
+    <div><h1>page2です</h1></div>
+  );
+};
